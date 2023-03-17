8398dec0ec03bc8c8d9a648fd393f3e3bab1fa0d
mikiui59 <54744152+mikiui59@users.noreply.github.com>
2023-03-17 11:54:18 +0000

    Update run_qa.py

--- examples/pytorch/question-answering/run_qa.py
+++ examples/pytorch/question-answering/run_qa.py
@@ -26,7 +26,7 @@ from typing import Optional
 
 import datasets
 import evaluate
-from datasets import load_dataset
+from datasets import load_dataset,load_from_disk
 from trainer_qa import QuestionAnsweringTrainer
 from utils_qa import postprocess_qa_predictions
 
@@ -285,12 +285,17 @@ def main():
     # download the dataset.
     if data_args.dataset_name is not None:
         # Downloading and loading a dataset from the hub.
-        raw_datasets = load_dataset(
-            data_args.dataset_name,
-            data_args.dataset_config_name,
-            cache_dir=model_args.cache_dir,
-            use_auth_token=True if model_args.use_auth_token else None,
-        )
+        try:
+            raw_datasets = load_dataset(
+                data_args.dataset_name,
+                data_args.dataset_config_name,
+                cache_dir=model_args.cache_dir,
+                use_auth_token=True if model_args.use_auth_token else None,
+            )
+        except:
+            raw_datasets = load_from_disk(
+              data_args.dataset_name
+            )
     else:
         data_files = {}
         if data_args.train_file is not None:
